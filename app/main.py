68ad83cc80f326b255823ecdd713ab37d93009ff
PlayWithDen4ik <velichkodenis34@gmail.com>
2026-01-31 20:19:05 +0200

    Solution with fix

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 def copy_file(command: str) -> None:
-    if command.startswith("cp"):
-        split_command = command.split()
-        if len(split_command) != 3:
-            return
-        _, source, dest = split_command
-        if source == dest:
-            return
-        try:
-            with open(source, "r") as file1, open(dest, "w") as file2:
-                file2.write(file1.read())
-        except FileNotFoundError:
-            print("No file found")
+    if not command:
+        return
+    split_command = command.split()
+    if len(split_command) != 3:
+        return
+    cmd, source, dest = split_command
+    if cmd != "cp":
+        return
+    if source == dest:
+        return
+    try:
+        with open(source, "r") as file1, open(dest, "w") as file2:
+            file2.write(file1.read())
+    except FileNotFoundError:
+        return
